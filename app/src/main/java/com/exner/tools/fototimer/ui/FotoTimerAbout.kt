436eae5c5e9ed303d5a1c7a556997fb42073cbc5
Jan Exner <jan.exner@gmx.net>
2023-02-16 15:22:00 +0100

    In which our hero fixes a URL, again

--- app/src/main/java/com/exner/tools/fototimer/ui/FotoTimerAbout.kt
+++ app/src/main/java/com/exner/tools/fototimer/ui/FotoTimerAbout.kt
@@ -2,6 +2,7 @@ package com.exner.tools.fototimer.ui
 
 import android.content.res.Configuration
 import android.view.ViewGroup
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
 import android.webkit.WebViewClient
 import androidx.compose.foundation.layout.*
@@ -9,6 +10,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,13 +60,31 @@ fun AboutScreenElements() {
     }
     Spacer(modifier = Modifier.height(8.dp))
     val url = "https://www.jan-exner.de/software/android/fototimer/about.html"
+    // second URL that reflects how kirby maps the above URL (sigh)
+    val url2 = "https://www.jan-exner.de/content/5-software/android/fototimer/about.html"
+    val uriHandler = LocalUriHandler.current
     AndroidView(factory = { context ->
         WebView(context).apply {
             layoutParams = ViewGroup.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT,
                 ViewGroup.LayoutParams.MATCH_PARENT
             )
-            webViewClient = WebViewClient()
+            webViewClient = object : WebViewClient() {
+                override fun shouldOverrideUrlLoading(
+                    view: WebView?,
+                    request: WebResourceRequest?
+                ): Boolean {
+                    if (request != null) {
+                        return if (request.url.toString() == url || request.url.toString() == url2) { // load this, and only this, URL in webview
+                            false
+                        } else { // load anything else in the Android default browser
+                            uriHandler.openUri(request.url.toString())
+                            true
+                        }
+                    }
+                    return true
+                }
+            }
             loadUrl(url)
         }
     }, update = {
