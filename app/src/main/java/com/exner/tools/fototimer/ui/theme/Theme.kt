87e91ddcf24d6e364340d314558846013751782f
Jan Exner <jan.exner@gmx.net>
2023-02-21 17:07:30 +0100

    In which our hero prepares uses colours suggested by MD3

--- app/src/main/java/com/exner/tools/fototimer/ui/theme/Theme.kt
+++ app/src/main/java/com/exner/tools/fototimer/ui/theme/Theme.kt
@@ -9,46 +9,69 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
 
-private val DarkColorScheme = darkColorScheme(
-    primary = md_jexner_alabaster_80,
-    onPrimary = md_jexner_alabaster_20,
-    primaryContainer = md_jexner_alabaster_30,
-    onPrimaryContainer = md_jexner_alabaster_90,
-    secondary = md_jexner_blue_80,
-    onSecondary = md_jexner_blue_20,
-    secondaryContainer = md_jexner_blue_30,
-    onSecondaryContainer = md_jexner_blue_90,
-    tertiary = md_jexner_orange_80,
-    onTertiary = md_jexner_orange_20,
-    tertiaryContainer = md_jexner_orange_30,
-    onTertiaryContainer = md_jexner_orange_90,
-    background = md_jexner_dim_gray_10,
-    onBackground = md_jexner_dim_gray_90,
-    surfaceVariant = md_jexner_khaki_30,
-    onSurfaceVariant = md_jexner_khaki_80,
-    outline = md_jexner_khaki_60,
-    outlineVariant = md_jexner_khaki_30,
+private val LightColors = lightColorScheme(
+    primary = md_theme_light_primary,
+    onPrimary = md_theme_light_onPrimary,
+    primaryContainer = md_theme_light_primaryContainer,
+    onPrimaryContainer = md_theme_light_onPrimaryContainer,
+    secondary = md_theme_light_secondary,
+    onSecondary = md_theme_light_onSecondary,
+    secondaryContainer = md_theme_light_secondaryContainer,
+    onSecondaryContainer = md_theme_light_onSecondaryContainer,
+    tertiary = md_theme_light_tertiary,
+    onTertiary = md_theme_light_onTertiary,
+    tertiaryContainer = md_theme_light_tertiaryContainer,
+    onTertiaryContainer = md_theme_light_onTertiaryContainer,
+    error = md_theme_light_error,
+    errorContainer = md_theme_light_errorContainer,
+    onError = md_theme_light_onError,
+    onErrorContainer = md_theme_light_onErrorContainer,
+    background = md_theme_light_background,
+    onBackground = md_theme_light_onBackground,
+    surface = md_theme_light_surface,
+    onSurface = md_theme_light_onSurface,
+    surfaceVariant = md_theme_light_surfaceVariant,
+    onSurfaceVariant = md_theme_light_onSurfaceVariant,
+    outline = md_theme_light_outline,
+    inverseOnSurface = md_theme_light_inverseOnSurface,
+    inverseSurface = md_theme_light_inverseSurface,
+    inversePrimary = md_theme_light_inversePrimary,
+    surfaceTint = md_theme_light_surfaceTint,
+    outlineVariant = md_theme_light_outlineVariant,
+    scrim = md_theme_light_scrim,
 )
 
-private val LightColorScheme = lightColorScheme(
-    primary = md_jexner_alabaster_40,
-    onPrimary = md_jexner_alabaster_100,
-    primaryContainer = md_jexner_alabaster_90,
-    onPrimaryContainer = md_jexner_alabaster_10,
-    secondary = md_jexner_blue_40,
-    onSecondary = md_jexner_blue_100,
-    secondaryContainer = md_jexner_blue_90,
-    onSecondaryContainer = md_jexner_blue_10,
-    tertiary = md_jexner_orange_40,
-    onTertiary = md_jexner_orange_100,
-    tertiaryContainer = md_jexner_orange_90,
-    onTertiaryContainer = md_jexner_orange_10,
-    background = md_jexner_dim_gray_99,
-    onBackground = md_jexner_dim_gray_10,
-    surfaceVariant = md_jexner_khaki_90,
-    onSurfaceVariant = md_jexner_khaki_30,
-    outline = md_jexner_khaki_50,
-    outlineVariant = md_jexner_khaki_80,
+
+private val DarkColors = darkColorScheme(
+    primary = md_theme_dark_primary,
+    onPrimary = md_theme_dark_onPrimary,
+    primaryContainer = md_theme_dark_primaryContainer,
+    onPrimaryContainer = md_theme_dark_onPrimaryContainer,
+    secondary = md_theme_dark_secondary,
+    onSecondary = md_theme_dark_onSecondary,
+    secondaryContainer = md_theme_dark_secondaryContainer,
+    onSecondaryContainer = md_theme_dark_onSecondaryContainer,
+    tertiary = md_theme_dark_tertiary,
+    onTertiary = md_theme_dark_onTertiary,
+    tertiaryContainer = md_theme_dark_tertiaryContainer,
+    onTertiaryContainer = md_theme_dark_onTertiaryContainer,
+    error = md_theme_dark_error,
+    errorContainer = md_theme_dark_errorContainer,
+    onError = md_theme_dark_onError,
+    onErrorContainer = md_theme_dark_onErrorContainer,
+    background = md_theme_dark_background,
+    onBackground = md_theme_dark_onBackground,
+    surface = md_theme_dark_surface,
+    onSurface = md_theme_dark_onSurface,
+    surfaceVariant = md_theme_dark_surfaceVariant,
+    onSurfaceVariant = md_theme_dark_onSurfaceVariant,
+    outline = md_theme_dark_outline,
+    inverseOnSurface = md_theme_dark_inverseOnSurface,
+    inverseSurface = md_theme_dark_inverseSurface,
+    inversePrimary = md_theme_dark_inversePrimary,
+    surfaceTint = md_theme_dark_surfaceTint,
+    outlineVariant = md_theme_dark_outlineVariant,
+    scrim = md_theme_dark_scrim,
 )
 
 @Composable
@@ -63,8 +86,8 @@ fun FotoTimerTheme(
             val context = LocalContext.current
             if (currentDarkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
-        currentDarkTheme -> DarkColorScheme
-        else -> LightColorScheme
+        currentDarkTheme -> DarkColors
+        else -> LightColors
     }
     MaterialTheme(
         colorScheme = colorScheme,
