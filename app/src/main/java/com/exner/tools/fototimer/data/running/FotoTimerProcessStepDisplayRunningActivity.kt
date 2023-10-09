37151550535fe0457e95eb7dfc9ebc698eca7a27
jexner <jan.exner@gmx.net>
2023-10-09 16:54:52 +0200

    In which our hero updates dependencies

---
+++ app/src/main/java/com/exner/tools/fototimer/data/running/FotoTimerProcessStepDisplayRunningActivity.kt
@@ -0,0 +1,7 @@
+package com.exner.tools.fototimer.data.running
+
+class FotoTimerProcessStepDisplayRunningActivity() : FotoTimerProcessStepActivity() {
+    override fun doActivity() {
+        TODO("Not yet implemented")
+    }
+}
