bab85a7771efef8446226e9535f8b7443d21ae00
jexner <jan.exner@gmx.net>
2023-11-17 11:44:02 +0100

    In which our hero entirely removes kapt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,16 +1,8 @@
 plugins {
     id("com.android.application")
-    id("com.google.dagger.hilt.android")
     id("kotlin-android")
-    id("kotlin-kapt")
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.9.20"
-    id("org.jetbrains.kotlin.android")
-
-    // compose destinations
-    id("com.google.devtools.ksp") version "1.9.20-1.0.14"
-    // before updating this, check compatibilities on https://developer.android.com/studio/releases#android_gradle_plugin_and_android_studio_compatibility
-    // and https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-    // and, obv https://github.com/google/ksp/releases
+    id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
 }
 
 android {
@@ -47,11 +39,6 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.4"
         // see https://developer.android.com/jetpack/androidx/releases/compose-kotlin
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
     buildToolsVersion = "34.0.0"
 }
@@ -100,7 +87,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.48.1")
     implementation("androidx.datastore:datastore-core:1.0.0")
     implementation("androidx.core:core-ktx:1.12.0")
-    kapt("com.google.dagger:hilt-compiler:2.48.1")
+    ksp("com.google.dagger:hilt-compiler:2.48.1")
     implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
 
     // Room components
@@ -109,30 +96,12 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.0")
 
     // others
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
 
     // compose destinations
     implementation("io.github.raamcosta.compose-destinations:core:1.9.54")
     ksp("io.github.raamcosta.compose-destinations:ksp:1.9.54")
-
-    // testing, which I don"t do
-    testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
-    // For instrumentation tests
-    androidTestImplementation("com.google.dagger:hilt-android-testing:2.48.1")
-    kaptAndroidTest("com.google.dagger:hilt-compiler:2.48.1")
-    // For local unit tests
-    testImplementation("com.google.dagger:hilt-android-testing:2.48.1")
-    kaptTest("com.google.dagger:hilt-compiler:2.48.1")
-}
-
-kapt {
-    correctErrorTypes = true
 }
 
 hilt {
