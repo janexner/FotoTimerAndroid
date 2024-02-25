d2f08a3525ebb1078f8c9856f9d86a0bfeb78449
jexner <jan.exner@gmx.net>
2024-02-25 16:31:00 +0100

    in which our hero goes edge to edge

--- app/src/main/java/com/exner/tools/fototimer/MainActivity.kt
+++ app/src/main/java/com/exner/tools/fototimer/MainActivity.kt
@@ -5,8 +5,14 @@ import android.os.Build
 import android.os.Bundle
 import android.os.Vibrator
 import android.os.VibratorManager
+import android.view.View
+import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.core.graphics.Insets
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.exner.tools.fototimer.audio.SoundPoolHolder
 import com.exner.tools.fototimer.audio.VibratorHolder
 import com.exner.tools.fototimer.data.preferences.FotoTimerPreferencesManager
@@ -17,19 +23,43 @@ import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
+
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
 
-    @Inject lateinit var userPreferencesManager: FotoTimerPreferencesManager
+    @Inject
+    lateinit var userPreferencesManager: FotoTimerPreferencesManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val forceNightMode = userPreferencesManager.nightMode()
 
+        // clear FLAG_TRANSLUCENT_STATUS flag:
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+        // add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(window.decorView) { v: View, insets: WindowInsetsCompat ->
+            val systemInsets: Insets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(
+                v.paddingLeft,
+                v.paddingTop,
+                v.paddingRight,
+                v.paddingBottom - systemInsets.bottom
+            ) // subtract the insets from the bottom padding
+            insets
+        }
+
         setContent {
+            // Update the edge to edge configuration to match the theme
+            // This is the same parameters as the default enableEdgeToEdge call, but we manually
+            // resolve whether or not to show dark theme using uiState, since it can be different
+            // than the configuration's dark theme value based on the user preference.
+            val darkMode = runBlocking { forceNightMode.first() }
             FotoTimerTheme(
-                darkTheme = runBlocking { forceNightMode.first() }
+                darkTheme = darkMode
             ) {
                 FotoTimerGlobalScaffold()
             }
