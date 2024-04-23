8fd10a1dfb120496748d78a577cf63632481e664
jexner <jan.exner@gmx.net>
2024-04-23 21:42:22 +0200

    in which our hero experiments with LazyVerticalGrid in Settings

--- app/src/main/java/com/exner/tools/fototimer/ui/destinations/Settings.kt
+++ app/src/main/java/com/exner/tools/fototimer/ui/destinations/Settings.kt
@@ -5,10 +5,13 @@ import android.os.Build
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
@@ -72,8 +75,8 @@ fun Settings(
         layoutAsTabs = false
     }
     // show settings
-    Column(modifier = Modifier.fillMaxSize()) {
-        if (layoutAsTabs) {
+    if (layoutAsTabs) {
+        Column(modifier = Modifier.fillMaxSize()) {
             TabRow(
                 selectedTabIndex = tabIndex
             ) {
@@ -159,114 +162,148 @@ fun Settings(
                     }
                 }
             }
-        } else {
-            // enough space to show everything at once
-            LazyVerticalGrid(
-                columns = GridCells.Adaptive(minSize = 300.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-                horizontalArrangement = Arrangement.spacedBy(16.dp),
+        }
+    } else {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(minSize = 300.dp),
+            contentPadding = PaddingValues(8.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            horizontalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            item (
+                span = { GridItemSpan(maxLineSpan) }
             ) {
-                item {
-                    TextAndSwitch(text = "Force night mode", checked = nightMode) {
-                        settingsViewModel.updateNightMode(it)
-                    }
+                Text(
+                    text = "UI",
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+            item {
+                TextAndSwitch(text = "Force night mode", checked = nightMode) {
+                    settingsViewModel.updateNightMode(it)
                 }
-
-                item {
-                    TextAndSwitch(
-                        text = "Use current interval time as central display, rather than current process time",
-                        checked = intervalTimeIsCentral
-                    ) {
-                        settingsViewModel.updateIntervalTimeIsCentral(it)
-                    }
+            }
+            item {
+                TextAndSwitch(
+                    text = "Use current interval time as central display, rather than current process time",
+                    checked = intervalTimeIsCentral
+                ) {
+                    settingsViewModel.updateIntervalTimeIsCentral(it)
                 }
+            }
 
-                item {
+            item (
+                span = { GridItemSpan(maxLineSpan) }
+            ) {
+                AnimatedVisibility(visible = expertMode) {
+                    Text(
+                        text = "Times",
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+
+            item {
+                AnimatedVisibility(visible = expertMode) {
+                    TextFieldForTimes(
+                        value = defaultProcessTime,
+                        label = { Text(text = "Default Process time") },
+                        onValueChange = {
+                            settingsViewModel.updateDefaultProcessTime(it)
+                        }
+                    ) { Text(text = "30") }
+                }
+            }
+
+            item {
+                AnimatedVisibility(visible = expertMode) {
+                    TextFieldForTimes(
+                        value = defaultIntervalTime,
+                        label = { Text(text = "Default Interval time") },
+                        onValueChange = {
+                            settingsViewModel.updateDefaultIntervalTime(it)
+                        },
+                    ) { Text(text = "10") }
+                }
+            }
+
+            item {
+                AnimatedVisibility(visible = expertMode) {
+                    TextFieldForTimes(
+                        value = defaultLeadInTime,
+                        label = { Text(text = "Default Lead-in time") },
+                        onValueChange = {
+                            settingsViewModel.updateDefaultLeadInTime(it)
+                        },
+                    ) { Text(text = "0") }
+                }
+            }
+
+            item {
+                AnimatedVisibility(visible = expertMode) {
+                    TextFieldForTimes(
+                        value = defaultPauseTime,
+                        label = { Text(text = "Default Pause time") },
+                        onValueChange = {
+                            settingsViewModel.updateDefaultPauseTime(it)
+                        },
+                    ) { Text(text = "5") }
+                }
+            }
+
+            item (
+                span = { GridItemSpan(maxLineSpan) }
+            ) {
+                AnimatedVisibility(visible = expertMode) {
+                    Text(
+                        text = "Sounds",
+                        style = MaterialTheme.typography.bodyLarge,
+                        modifier = Modifier.padding(16.dp)
+                    )
+                }
+            }
+
+            item {
+                AnimatedVisibility(visible = expertMode) {
+                    OutlinedTextField(
+                        value = numberOfPreBeeps.toString(),
+                        label = { Text(text = "Pre-beeps (small beeps before the interval)") },
+                        modifier = Modifier.fillMaxWidth(),
+                        singleLine = true,
+                        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                        onValueChange = {
+                            val newPreBeeps = it.toIntOrNull() ?: 0
+                            settingsViewModel.updateNumberOfPreBeeps(newPreBeeps)
+                        },
+                        placeholder = { Text(text = "4") },
+                        textStyle = MaterialTheme.typography.bodyLarge
+                    )
+                }
+            }
+
+            item {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
                     AnimatedVisibility(visible = expertMode) {
-                        TextFieldForTimes(
-                            value = defaultProcessTime,
-                            label = { Text(text = "Default Process time") },
-                            onValueChange = {
-                                settingsViewModel.updateDefaultProcessTime(it)
-                            }
-                        ) { Text(text = "30") }
-                    }
-                }
-
-                item {
-                    AnimatedVisibility(visible = expertMode) {
-                        TextFieldForTimes(
-                            value = defaultIntervalTime,
-                            label = { Text(text = "Default Interval time") },
-                            onValueChange = {
-                                settingsViewModel.updateDefaultIntervalTime(it)
-                            },
-                        ) { Text(text = "10") }
-                    }
-                }
-
-                item {
-                    AnimatedVisibility(visible = expertMode) {
-                        TextFieldForTimes(
-                            value = defaultLeadInTime,
-                            label = { Text(text = "Default Lead-in time") },
-                            onValueChange = {
-                                settingsViewModel.updateDefaultLeadInTime(it)
-                            },
-                        ) { Text(text = "0") }
-                    }
-                }
-
-                item {
-                    AnimatedVisibility(visible = expertMode) {
-                            TextFieldForTimes(
-                                value = defaultPauseTime,
-                                label = { Text(text = "Default Pause time") },
-                                onValueChange = {
-                                    settingsViewModel.updateDefaultPauseTime(it)
-                                },
-                            ) { Text(text = "5") }
+                        TextAndSwitch(
+                            text = "Vibrate when playing sounds",
+                            checked = vibrateEnabled
+                        ) {
+                            settingsViewModel.updateVibrateEnabled(it)
                         }
                     }
-
-                item {
-                    AnimatedVisibility(visible = expertMode) {
-                        OutlinedTextField(
-                            value = numberOfPreBeeps.toString(),
-                            label = { Text(text = "Pre-beeps (small beeps before the interval)") },
-                            modifier = Modifier.fillMaxWidth(),
-                            singleLine = true,
-                            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-                            onValueChange = {
-                                val newPreBeeps = it.toIntOrNull() ?: 0
-                                settingsViewModel.updateNumberOfPreBeeps(newPreBeeps)
-                            },
-                            placeholder = { Text(text = "4") },
-                            textStyle = MaterialTheme.typography.bodyLarge
-                        )
-                    }
                 }
+            }
 
-                item {
-                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                        AnimatedVisibility(visible = expertMode) {
-                            TextAndSwitch(
-                                text = "Vibrate when playing sounds",
-                                checked = vibrateEnabled
-                            ) {
-                                settingsViewModel.updateVibrateEnabled(it)
-                            }
-                        }
-                    }
-                }
-
-                item {
-                    TextAndSwitch(
-                        text = "Expert mode (more options everywhere)",
-                        checked = expertMode
-                    ) {
-                        settingsViewModel.updateExpertMode(it)
-                    }
+            item (
+                span = { GridItemSpan(maxLineSpan) }
+            ) {
+                TextAndSwitch(
+                    text = "Expert mode (more options everywhere)",
+                    checked = expertMode
+                ) {
+                    settingsViewModel.updateExpertMode(it)
                 }
             }
         }
