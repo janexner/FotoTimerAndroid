a33b21ffab5130509dbaae413e55b171c9e690c7
jexner <jan.exner@gmx.net>
2024-05-22 16:24:29 +0200

    in which our hero updates fastlane metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.exner.tools.fototimer"
         minSdk = 29
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.0"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
