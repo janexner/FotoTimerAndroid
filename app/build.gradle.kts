0e8d7363f80b22b1d8b9f562bf5af13fa37a173d
jexner <jan.exner@gmx.net>
2024-05-24 00:55:35 +0200

    in which our hero tries even more things for F-Droid

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,9 +25,10 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = true
-            isShrinkResources = true
+            isMinifyEnabled = false
+            isShrinkResources = false
             isDebuggable = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
         debug {
             isDebuggable = true
