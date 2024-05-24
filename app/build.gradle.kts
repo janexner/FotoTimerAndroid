6f6e6f9540c2104b08c8184019aa8a13b91bc144
jexner <jan.exner@gmx.net>
2024-05-24 09:56:01 +0200

    in which our hero ups versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.exner.tools.fototimer"
         minSdk = 29
         targetSdk = 34
-        versionCode = 4
-        versionName = "1.0.2"
+        versionCode = 5
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
