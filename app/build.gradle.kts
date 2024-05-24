eb93b61d65d031767d22857835a6438c201d6a60
jexner <jan.exner@gmx.net>
2024-05-24 09:56:49 +0200

    in which our hero re-enables Proguard/R8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
-            isShrinkResources = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             isDebuggable = false
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
