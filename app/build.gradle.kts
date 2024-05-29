98cc2fce191a514a542f5ca7400237d6bd6c3f77
jexner <jan.exner@gmx.net>
2024-05-29 11:30:22 +0200

    in which our hero removes betas or previews, and adds release to gitignore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.exner.tools.fototimer"
         minSdk = 29
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.0.3"
+        versionCode = 6
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
