2e0e5abe33e6369a1109f3d90bcdea934130f9b5
jexner <jan.exner@gmx.net>
2024-10-18 11:51:47 +0200

    in which our hero goes to Compose Destinations 2.1

--- app/src/main/java/com/exner/tools/fototimer/ui/destinations/FotoTimerGlobalScaffold.kt
+++ app/src/main/java/com/exner/tools/fototimer/ui/destinations/FotoTimerGlobalScaffold.kt
@@ -19,28 +19,29 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavHostController
 import com.ramcosta.composedestinations.DestinationsNavHost
 import com.ramcosta.composedestinations.generated.NavGraphs
 import com.ramcosta.composedestinations.generated.destinations.AboutDestination
 import com.ramcosta.composedestinations.generated.destinations.ProcessListDestination
 import com.ramcosta.composedestinations.generated.destinations.ProcessRunDestination
 import com.ramcosta.composedestinations.generated.destinations.SettingsDestination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.dependency
-import com.ramcosta.composedestinations.navigation.navigate
 import com.ramcosta.composedestinations.rememberNavHostEngine
 import com.ramcosta.composedestinations.spec.DestinationSpec
 import com.ramcosta.composedestinations.utils.currentDestinationAsState
+import com.ramcosta.composedestinations.utils.rememberDestinationsNavigator
 
 @Composable
 fun FotoTimerGlobalScaffold(windowSizeClass: WindowSizeClass) {
     val engine = rememberNavHostEngine()
     val navController = engine.rememberNavController()
+    val destinationsNavigator = navController.rememberDestinationsNavigator()
     val destination = navController.currentDestinationAsState().value
 
     Scaffold(
         topBar = {
-            FotoTimerTopBar(destination, navController)
+            FotoTimerTopBar(destination, destinationsNavigator)
         },
         content = { innerPadding ->
             val newPadding = PaddingValues.Absolute(
@@ -55,7 +56,8 @@ fun FotoTimerGlobalScaffold(windowSizeClass: WindowSizeClass) {
                 dependenciesContainerBuilder = {
                     dependency(windowSizeClass)
                 },
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
                     .consumeWindowInsets(newPadding)
                     .padding(newPadding)
             ) {
@@ -68,7 +70,7 @@ fun FotoTimerGlobalScaffold(windowSizeClass: WindowSizeClass) {
 @Composable
 private fun FotoTimerTopBar(
     destination: DestinationSpec?,
-    navController: NavHostController
+    navigator: DestinationsNavigator
 ) {
     TopAppBar(
         title = { Text(text = "Foto Timer") },
@@ -83,7 +85,7 @@ private fun FotoTimerTopBar(
                 }
 
                 else -> {
-                    IconButton(onClick = { navController.navigateUp() }) {
+                    IconButton(onClick = { navigator.navigateUp() }) {
                         Icon(
                             imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                             contentDescription = "Back"
@@ -95,7 +97,7 @@ private fun FotoTimerTopBar(
         actions = {
             when (destination) {
                 SettingsDestination -> {
-                    AboutActionIconButton(navController)
+                    AboutActionIconButton(navigator)
                 }
 
                 AboutDestination -> {
@@ -103,8 +105,8 @@ private fun FotoTimerTopBar(
                 }
 
                 else -> {
-                    SettingsActionIconButton(navController)
-                    AboutActionIconButton(navController)
+                    SettingsActionIconButton(navigator)
+                    AboutActionIconButton(navigator)
                 }
             }
         }
@@ -112,18 +114,18 @@ private fun FotoTimerTopBar(
 }
 
 @Composable
-private fun AboutActionIconButton(navController: NavHostController) {
+private fun AboutActionIconButton(navigator: DestinationsNavigator) {
     IconButton(onClick = {
-        navController.navigate(AboutDestination())
+        navigator.navigate(AboutDestination())
     }) {
         Icon(imageVector = Icons.Filled.Info, contentDescription = "About Foto Timer")
     }
 }
 
 @Composable
-private fun SettingsActionIconButton(navController: NavHostController) {
+private fun SettingsActionIconButton(navigator: DestinationsNavigator) {
     IconButton(onClick = {
-        navController.navigate(SettingsDestination())
+        navigator.navigate(SettingsDestination())
     }) {
         Icon(
             imageVector = Icons.Filled.Settings,
